fb3deb6d181204dd52a25e82e5a4472e420e177b
akib1162100 <35798840+akib1162100@users.noreply.github.com>
2022-03-08 17:57:20 +0600

    utter_msg and story attempt1

--- chatbot.py
+++ chatbot.py
@@ -6,34 +6,31 @@ from __future__ import unicode_literals, print_function
 import io
 import random
 import string
-from venv import create # to process standard python strings
-import warnings
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+import json
+import nltk
+import pandas as pd
+import warnings
 
 #for snips
 
-import io
-import json
+from venv import create # to process standard python strings
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from snips_nlu import SnipsNLUEngine
 from snips_nlu.default_configs import CONFIG_EN
+from nltk.stem import WordNetLemmatizer
+from stories_responses import *
+
+warnings.filterwarnings('ignore')
+
+nltk.download('popular', quiet=True) # for downloading packages
 
 with io.open("dataset.json") as f:
     sample_dataset = json.load(f)
 
 nlu_engine = SnipsNLUEngine(config=CONFIG_EN)
 nlu_engine = nlu_engine.fit(sample_dataset)
-
-
-import pandas as pd
-import warnings
-warnings.filterwarnings('ignore')
-
-import nltk
-from nltk.stem import WordNetLemmatizer
-nltk.download('popular', quiet=True) # for downloading packages
-
 # uncomment the following only the first time
 #nltk.download('punkt') # first-time use only
 #nltk.download('wordnet') # first-time use only
@@ -61,15 +58,51 @@ GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
 GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
 
 
-
-
-
 log = None
-STORIES = {
-    'check_balance': {'0':'utter_ask_account','1':{'inform':{'account':76534721}},'2':'utter_tell_account','3':'utter_ask_pin','3':{'inform':{'pin':3276}},'4':'utter_tell_balance'}
+all_slots = {
+    'account':None,
+    'pin':None,
+    'amount_of_money':None,
+    'yes_no':False
+}
+
+slot_map_for_ban = {
+    'account':'একাউন্ট নাম্বার',
+    'pin':'পিন নাম্বার',
+    'amount_of_money':'টাকার পরিমান',
+    'yes_no':'হ্যাঁ বা না'   
+}
+
+STORIES = {                          ##utter                      action
+    # 'check_balance': {'0':'utter_ask_account','1':{'inform':[{'account':76534721}]},'2':'utter_tell_account','3':{''},'3':'utter_ask_pin','3':{'inform':[{'pin':3276}]},'4':'utter_tell_balance'},
+    'check_balance': {'0':{'name':'utter_ask_account'},
+        '1':{'name':'action_get_account','expected_intents':['inform'],'req_slots':['account'],'prev_step':None},
+        '2':{'name':'utter_tell_account'},
+        '3':{'name':'action_yes_no','expected_intents':['affirm','deny'],'req_slots':['yes_no'],'prev_step':'1'},
+        '4':{'name':'utter_ask_pin'},
+        '5':{'name':'action_get_pin','expected_intents':['inform'],'req_slots':['pin'],'prev_step':None},
+        '6':{'name':'utter_tell_pin'},
+        '7':{'name':'action_yes_no','expected_intents':['affirm','deny'],'req_slots':['yes_no'],'prev_step':'7'},
+        '8':{'name':'utter_tell_balance'}}
 }
 
 
+def utters_responses(to_do=None,slot=None):
+    if(to_do=='ask'):
+        print("দয়া করে "+slot_map_for_ban[slot]+" দিন")
+    elif(to_do=='tell'):
+        print("আপনার "+slot_map_for_ban[slot]+" হল "+str(all_slots[slot]))
+        print("এটা কি সঠিক হয়েছে?")
+    else:
+        print(" আমি ঠিক বুঝতে পারিনি। দয়া করে আবার বলুন")
+
+
+def utter_process(utter_name):
+    utter_parts=utter_name.spilt('_')
+    if(utter_parts[0]=='utter'):
+        user_response(to_do=utter_parts[1],slot=utter_parts[2])
+    else:
+        user_response()
 
 def greeting(sentence):
     """If user's input is a greeting, return a greeting response"""
@@ -107,11 +140,19 @@ while(flag==True):
     user_response = input()
     user_response=user_response.lower()
     if(user_response!='bye'):
-
         if(user_response=='thanks' or user_response=='thank you' ):
             flag=False
             print("ROBO: You are welcome..")
         
+        if (user_response=='check balance'):
+            story_intents = []
+            slots = []
+            this_story = STORIES['check_balance']
+            for key, value in STORIES.items():
+                if value['name'].split('_')[0] == 'utter':
+                    utter_process(value['name'])
+
+
         else:
             if(greeting(user_response)!=None):
                 print("ROBO: "+greeting(user_response))
--- stories_responses.py
+++ stories_responses.py
@@ -2,6 +2,19 @@ import yaml
 from yaml.loader import SafeLoader
 
 # Open the file and load the file
+intents= []
+utters = []
 with open('domain.yml', encoding="utf8") as f:
     data = yaml.load(f, Loader=SafeLoader)
-    print(data)
+    # print(data['intents'])
+    for intent in data['intents']:
+        if type(intent) is dict:
+            intents.extend(intent.keys())
+        else:
+            intents.append(intent)
+    # print(data['responses'])
+    for key, value in data['responses'].items():
+        dictionary ={}
+        dictionary ={key:value[0]['text']}
+        utters.append(dictionary)
+    print(utters)
